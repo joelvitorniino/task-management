59c73218e9a724c0569093ff0a7b39a42f7d32ca
joelvitorniino <joelprofessor13@gmail.com>
2023-04-02 19:00:17 -0300

    feat: roboto bold on page login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,13 @@
 export default function Login() {
     return (
         <div className="flex flex-col justify-center items-center">
-            <label htmlFor="email">Email:</label>
+            <label htmlFor="email" className="font-bold">Email:</label>
             <input type="email" name="email" className="bg-whiteSlate ml-1" />
             
-            <label htmlFor="email">Password:</label>
+            <label htmlFor="email" className="font-bold">Password:</label>
             <input type="password" name="password" className="bg-whiteSlate ml-1" />
 
-            <button className="bg-whiteSlate px-3 mt-2">Login</button>
+            <button className="bg-whiteSlate px-3 mt-2 font-bold">Login</button>
         </div>
     )
 }

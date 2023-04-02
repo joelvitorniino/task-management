1c3d7eceb18f31e458050d10dce91c2414666efc
joelvitorniino <joelprofessor13@gmail.com>
2023-04-02 18:54:42 -0300

    feat: load roboto fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,19 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from './components/Layout'
 
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500', '700']
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={roboto.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   )
 }
